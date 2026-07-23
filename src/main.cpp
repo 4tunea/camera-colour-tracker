62e83c84493c229f58126b129f4ec89e09abf853
Robertas <Robertas@Mackus.lt>
2026-07-23 18:24:15 +0300

    Add command-line input validation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
-int main(){
-    std::cout << "Robots!\n";
+void displayProgramInformation(){
+    std::cout<<"Camera colour tracker!\n";
+    std::cout<<"Usage: colour_tracker <image_path>\n";
+}
+
+int main(int argc, char* argv[]){
+    displayProgramInformation();
+
+    if (argc < 2){
+        std::cerr<<"Error: no image path provided.\n";
+        return 1;
+    }
+
+    const char* imagePath = argv[1];
+
+    std::cout<<"Provided image path: "<< imagePath << '\n';
+
     return 0;
 }
